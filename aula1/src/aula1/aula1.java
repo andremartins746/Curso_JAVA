21761e9a5169b58b374e77f2a503f79825c1066f
André Martins Pereira <64427613+andremartins746@users.noreply.github.com>
2021-02-24 08:10:27 -0300

    Add files via upload

---
+++ aula1/src/aula1/aula1.java
@@ -0,0 +1,21 @@
+package aula1;
+
+public class aula1 {
+
+	public static void main(String[] args) {
+		
+		Carro gol = new Carro();
+		Carro passat = new Carro();
+		
+		gol.modelo = "gol";
+		passat.modelo = "passat";
+		
+		gol.painel();
+		passat.painel();
+		
+		
+		 
+	}
+
+}
+
